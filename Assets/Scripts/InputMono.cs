83da6f5fda0258ba24e4b283e8cbc045a7333d85
vanhadinh <ha.dinh@gihot.vn>
2020-11-13 10:50:53 +0700

    init project

---
+++ Assets/Scripts/InputMono.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputMono : MonoBehaviour
+{
+    public static string VALUE_KEY = "VALUE_KEY";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
